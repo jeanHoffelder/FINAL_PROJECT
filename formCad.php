d66d7744e32df7924559ed5b44ec83290d1cd9c3
jeanHoffelder <jl6934807@outlook.com>
2022-11-30 08:35:53 -0300

    up

--- formCad.php
+++ formCad.php
@@ -21,19 +21,19 @@ if(isset($_POST['botao'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="formCad.css" />
-    <title>Adiciona Atleta</title>
+    <title>Sing Up</title>
 </head>
 <body>
     <form action='formCad.php' method='POST'>
-        Nome:<br> <input name='nome' type='text' placeholder='Ex.: Thaciano' required>
+        Name:<br> <input name='nome' type='text' placeholder='Ex.: Thaciano' required>
         <br>
         E-mail:<br> <input name='email' type='email' placeholder='thaciano@gmail.com' required>
         <br>
-        Senha:<br> <input name='senha' type='password' placeholder='*****' required>
+        Password:<br> <input name='senha' type='password' placeholder='*****' required>
         <br>
-        Data de Nascimento:<br> <input name='data_nasc' type='date' required>
+        Birth date:<br> <input name='data_nasc' type='date' required>
         <br>
-        Turma:<br>
+        Class:<br>
         <select name='turma'> 
             <option value='0'>Select</option>
             <option value='ti1'>1° ano - Técnico em Informática</option>
@@ -50,30 +50,28 @@ if(isset($_POST['botao'])){
             <option value='tma4'>4° ano - Técnico em Meio Ambiente</option>
         </select>
         <br>
-        Altura:<br> <input name='altura' type='text' maxLength='3' placeholder='Ex.: 180' required>
+        Height:<br> <input name='altura' type='text' maxLength='3' placeholder='Ex.: 180' required>
         <br>
-        Posição:<br> <select name='posicao'>
-            <option value='0'>Select</option>
-            <option value='goleiro'>Goleiro</option>
-            <option value='fixo'>Fixo</option>
-            <option value='alaDireita'>Ala-Direita</option>
-            <option value='alaEsquerda'>Ala-Esquerda</option>
-            <option value='pivo'>Pivô</option>
+        Position:<br> <select name='posicao'>
+            <option value='goleiro'>Goalkeeper</option>
+            <option value='fixo'>Fixed</option>
+            <option value='alaDireita'>Right-Wing</option>
+            <option value='alaEsquerda'>Left-Wing</option>
+            <option value='pivo'>Target</option>
         </select>
         <br>
-        Sexo:<br> <select name='sexo'>
-            <option value='0'>Select</option>
+        Sex:<br> <select name='sexo'>
             <option value='m'>M</option>
             <option value='f'>F</option>
         </select>
         <br>
-        Foto:<br> <input name='foto' type='file' required>
+        Photo:<br> <input name='foto' type='file' required>
         <br>
         <input type='submit' name='botao'>
     </form>
 
     <img src="bemvindo.jpg" alt="">
-    <h1>BEM VINDO!</h1>
+    <h1>Welcome</h1>
 </body>
 </html>
 
--- restrita.php
+++ restrita.php
@@ -14,11 +14,11 @@ $atletas = Atleta::findall();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleRestrita.css" />
-    <title>Página do Time</title>
+    <title>Team Page</title>
 </head>
 <body>
     <img src="logo.png" alt="">
-    <h1>Você ainda não foi escalado :'(</h1>
+    <h1>You haven't been cast yet :'(</h1>
     <img src="logo.png" alt="">
 </body>
 </html>
