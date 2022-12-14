f649c0de410de4759e44f9cef2064c0ef4fa1ab7
jeanHoffelder <jl6934807@outlook.com>
2022-12-14 08:18:26 -0300

    up

--- restritaAdmin.php
+++ restritaAdmin.php
@@ -3,19 +3,35 @@
 session_start();
 if(!isset($_SESSION['id'])){
     header("location:index.php");
-}else{}
-
-
+}
 require_once __DIR__."/vendor/autoload.php";
 
 $atletas = Atleta::findall();
 $time = Time::findall();
+
+
 ?>
-
-
-
-echo(save)
-
-
-
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="styleRestrita.css" />
+    <title>Team Page</title>
+</head>
+<body>
+    <header>
+        <?php echo "<h2>Welcome, {$_SESSION['nome']} </h2>"?>
+    </header>
+    <img src="logo.png" alt="">
+    <h1>You haven't been cast yet :'(</h1>
+    <a href='sair.php'>Logout</a>
+    <?php
+    echo "<a href=formEdit.php?id={$_SESSION['id']}>Editar Perfil</a>";
+    ?>
+    <img src="logo.png" alt="">
+</body>
+</html>
+?>
