8c455642bf85a3d3fee56f120a71e6a691e3afe5
marceloost <104876213+marceloost@users.noreply.github.com>
2022-12-18 00:12:54 -0300

    up

--- testando.php
+++ testando.php
@@ -56,15 +56,43 @@ foreach($atletas as $atleta){
     $idade = calcularIdade($atleta->getData_nasc());
     $turma = $atleta->getTurma();
 
-    echo '<div class="card">';
-    echo '  <img src="atleta.jpg" alt="Foto do atleta">';
-    echo '  <h1>' . $nome . '</h1>';
-    echo '  <p>Posição: ' . $posicao . '</p>';
-    echo '  <p>Idade: ' . $idade . ' anos</p>';
-    echo '  <p>Turma: ' . $turma . '</p>';
-    echo '</div>';
-}
+    echo "<div id='container'>";
+    echo    "<div class='card' draggable='true' id='card-1'><img src='atleta.jpg' alt='Foto do atleta'>
+            <h1> $nome </h1>
+            <p>Posição: $posicao </p>
+            <p>Idade: $idade anos</p>
+            <p>Turma: $turma </p>
+            </div>
+            </div>";
+            ?>
+            <script>
+            // Obtém o container onde os cards serão arrastados
+            var container = document.getElementById('container');
 
+            // Adiciona os ouvintes de eventos de arrastar e soltar ao container
+            container.addEventListener('dragstart', dragStart);
+            container.addEventListener('dragover', dragOver);
+            container.addEventListener('drop', dragDrop);
+
+            // Função chamada quando o usuário começa a arrastar um card
+            function dragStart(e) {
+                e.dataTransfer.setData('text/plain', e.target.id);
+            }
+
+            // Função chamada quando o usuário arrasta um card sobre outro
+            function dragOver(e) {
+                e.preventDefault();
+            }
+
+            // Função chamada quando o usuário solta um card em outro
+            function dragDrop(e) {
+                var id = e.dataTransfer.getData('text');
+                var card = document.getElementById(id);
+                e.target.parentNode.insertBefore(card, e.target);
+            }
+            </script>
+            <?php
+} 
 ?>
 
 </script>
