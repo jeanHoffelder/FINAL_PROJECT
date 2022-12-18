e0810fc679f36540e34c4bcebad60dc5d6a440e9
marceloost <104876213+marceloost@users.noreply.github.com>
2022-12-18 01:05:50 -0300

    up

--- testando.php
+++ testando.php
@@ -50,52 +50,52 @@ function calcularIdade($data){
        return $idade;
    } 
 // Recebe as informações do atleta
+echo "<div id='container'>";
 foreach($atletas as $atleta){
     $nome = $atleta->getNome();
     $posicao = $atleta->getPosicao();
     $idade = calcularIdade($atleta->getData_nasc());
     $turma = $atleta->getTurma();
+    $id = $atleta->getId();
 
-    echo "<div id='container'>";
-    echo    "<div class='card' draggable='true' id='card-1'><img src='atleta.jpg' alt='Foto do atleta'>
-            <h1> $nome </h1>
-            <p>Posição: $posicao </p>
-            <p>Idade: $idade anos</p>
-            <p>Turma: $turma </p>
-            </div>
-            </div>";
-            ?>
-            <script>
-            // Obtém o container onde os cards serão arrastados
-            var container = document.getElementById('container');
+    echo "<div class='card' draggable='true' id='card-1'>";
+    echo "<img src='atleta.jpg' alt='Foto do atleta'>";
+    echo "<h1> $nome </h1>";
+    echo "<p>Posição: $posicao </p>";
+    echo "<p>Idade: $idade anos</p>";
+    echo "<p>Turma: $turma </p>";
+    echo "</div>";
 
-            // Adiciona os ouvintes de eventos de arrastar e soltar ao container
-            container.addEventListener('dragstart', dragStart);
-            container.addEventListener('dragover', dragOver);
-            container.addEventListener('drop', dragDrop);
-
-            // Função chamada quando o usuário começa a arrastar um card
-            function dragStart(e) {
-                e.dataTransfer.setData('text/plain', e.target.id);
-            }
-
-            // Função chamada quando o usuário arrasta um card sobre outro
-            function dragOver(e) {
-                e.preventDefault();
-            }
-
-            // Função chamada quando o usuário solta um card em outro
-            function dragDrop(e) {
-                var id = e.dataTransfer.getData('text');
-                var card = document.getElementById(id);
-                e.target.parentNode.insertBefore(card, e.target);
-            }
-            </script>
-            <?php
 } 
+echo "</div>";
 ?>
+<script>
+    // Obtém o container onde os cards serão arrastados
+    var container = document.getElementById('container');
 
+    // Adiciona os ouvintes de eventos de arrastar e soltar ao container
+    container.addEventListener('dragstart', dragStart);
+    container.addEventListener('dragover', dragOver);
+    container.addEventListener('drop', dragDrop);
+
+    // Função chamada quando o usuário começa a arrastar um card
+    function dragStart(e) {
+        e.dataTransfer.setData('text/plain', e.target.id);
+    }
+
+    // Função chamada quando o usuário arrasta um card sobre outro
+    function dragOver(e) {
+        e.preventDefault();
+    }
+
+    // Função chamada quando o usuário solta um card em outro
+    function dragDrop(e) {
+         var id = e.dataTransfer.getData('text');
+        var card = document.getElementById(id);
+        e.target.parentNode.insertBefore(card, e.target);
+    }
 </script>
-</script>
+
+
 </body>
 </html>
