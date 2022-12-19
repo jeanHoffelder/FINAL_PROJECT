8db01817f17414a92d4fd67f00d37dd52ff9d970
marceloost <104876213+marceloost@users.noreply.github.com>
2022-12-18 22:00:33 -0300

    up

--- telaTime.php
+++ telaTime.php
@@ -7,10 +7,35 @@ if(!isset($_SESSION['id'])){
     header("location:index.php");
 }
 
+<<<<<<< HEAD
 if (isset($_SESSION['blocked_user_id']) && $_SESSION['blocked_user_id'] == $user_id) {
   // Exiba uma mensagem informando que o usuário não tem permissão para realizar essa ação
   echo 'Você não tem permissão para realizar essa ação.';
 }
+=======
+
+// //php de criação para o time
+// //jogador1 = $this.idAtleta algo assim
+
+
+// public function __construct(private string $nome_time,private int $id_goleiro,
+//  private int $id_fixo, private int $id_alaDireita, private int $id_alaEsquerda, private int $id_Pivo, 
+//  private int $id_reserva1, private int $id_reserva2, private int $id_reserva3, private int $id_reserva4, 
+//  private int $id_reserva5){
+// }
+
+
+if(isset($_POST['botao'])){
+  require_once __DIR__."/vendor/autoload.php";
+  $time = new Time($_POST['nome'],$_POST['goleiro'],$_POST['fixo'],$_POST['ala_dir'],$_POST['ala_esq'],$_POST['pivo'],$_POST['reserva1'],$_POST['reserva2'],$_POST['reserva3'],$_POST['reserva4'],$_POST['reserva5']);
+  $time->save();
+  header("location: index.php");
+}
+
+
+
+
+>>>>>>> 50cde5034160fcc31db290fc4ee26dffcd150af2
 ?>
 <html>
 <head>
@@ -69,6 +94,7 @@ function calcularIdade($data){
 echo "<div id='container'>";
 
 foreach($atletas as $atleta){
+  $foto = $atleta->getFoto();
   $nome = $atleta->getNome();
   $posicao = $atleta->getPosicao();
   $idade = calcularIdade($atleta->getData_nasc());
@@ -77,7 +103,7 @@ foreach($atletas as $atleta){
 
   echo "<div class='card' draggable='true' id=$id>";
     echo "<div class='elementos_card'>";
-      echo "<img src='atleta.jpg' alt='Foto do atleta'>";
+      echo " <img width=150px src={$foto}>";
       echo "<h1> $nome </h1>";
       echo "<p>Posição: $posicao </p>";
       echo "<p>Idade: $idade anos</p>";
@@ -115,6 +141,37 @@ echo "</div>";
     }
   }
 </script>
+
+<br>
+
+    <div class='container2'>
+      <form action="telaTime.php" method='POST'>
+        nome do time: <input name='nome' type='text' placeholder='Ex: gremio' required>
+        <br>
+        Goleiro: <input name='goleiro' required>
+        <br>
+        Fixo: <input name='fixo' required>
+        <br>
+        Ala Direita: <input name='ala_dir' required>
+        <br>
+        Ala Esquerda: <input name='ala_esq' required>
+        <br>
+        Pivo: <input name='pivo' required>
+        <br>
+        Primeiro Reserva: <input name='reserva1' required>
+        <br>
+        Segundo Reserva: <input name='reserva2' required>
+        <br>
+        Terceiro Reserva: <input name='reserva3' required>
+        <br>
+        Quarto Reserva: <input name='reserva4' required>
+        <br>
+        Quinto Reserva: <input name='reserva5' required>
+
+
+        <input type='submit' name='botao' value='create team'>
+      </form>
+    </div>
 
 </body>
 </html>
