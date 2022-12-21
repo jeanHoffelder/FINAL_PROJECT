bbff5b91640df8d7f0ae1d8cc1561d8bd498ea37
jeanHoffelder <jl6934807@outlook.com>
2022-12-20 23:41:54 -0300

    up

--- telaTime.php
+++ telaTime.php
@@ -1,10 +1,14 @@
 <?php
 require_once __DIR__."/vendor/autoload.php";
 
+if (isset($_GET['conteudo']) && $_GET['conteudo'] == 'nao') {
+  $atletas = Atleta::findall();
+} else {
+  $atletas = Atleta::findall2(); 
+}
 
 
 
-$atletas = Atleta::findall2(); 
 
 session_start();
 if(!isset($_SESSION['id'])){
